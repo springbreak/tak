7465e142d84ad23c54d6bb3a78cc7ec853dcf10c
njir <njir@naver.com>
2014-06-01 12:36:58 +0900

    2  Test
    Junit

--- src/main/java/UserDaoTest.java
+++ src/main/java/UserDaoTest.java
@@ -1,15 +1,17 @@
 import java.sql.SQLException;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
 
-public class UserDaoTest {
-	public static void main(String[] args) throws ClassNotFoundException,
-			SQLException {
-		//UserDao dao = new DaoFactory().userDao();
+public class UserDaoTest {	
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {
 		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
@@ -19,15 +21,10 @@ public class UserDaoTest {
 		user.setPassword("married");
 
 		dao.add(user);
-
-		System.out.println(user.getId() + " 등록 성공");
-
+		
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		// this is comment
-		System.out.println(user2.getPassword());
-
-		System.out.println(user2.getId() + " 조회 성공");
-
+		
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
 	}
 }
